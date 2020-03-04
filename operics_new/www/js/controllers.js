1fda20d3bef2bd01ec824f4130bde678471df746
Burç ÖZARI <43642312+brckhan88@users.noreply.github.com>
2020-03-05 00:15:16 +0300

    aa

--- operics_new/www/js/controllers.js
+++ operics_new/www/js/controllers.js
@@ -27,22 +27,16 @@ angular.module('starter.controllers', [])
         console.log($scope.x);
     }
    
-    $scope.operics = [
-
-    {
-
-        tr: [{
-
-            references: [
-                { img: 'https://i4.hurimg.com/i/hurriyet/75/0x0/5cd01f8ec03c0e2e30c221b5.jpg', id: 0 },
-                { img: 'https://i.pinimg.com/originals/00/82/79/0082797f78fadd08c5bd2926a450667f.png', id: 1 },
-                { img: 'https://i.pinimg.com/originals/e9/b6/be/e9b6be54cbf873b14681a21dca530f22.png', id: 2 },
-                { img: 'https://c-es.nl/wp-content/uploads/2019/07/2019-07-07-Microsoft-Logo.png', id: 3 },
-                { img: 'https://www.aboomi.com/wp-content/uploads/enerjisa.png', id: 4 },
-                { img: 'https://www.pendiklitv.com/wp-content/uploads/2018/08/%C4%B0SK%C4%B0-logo.jpg', id: 5 }
-            ]
-        }]
-    ]};
+    $scope.operics = [{
+        references :[
+            { img: "https://i4.hurimg.com/i/hurriyet/75/0x0/5cd01f8ec03c0e2e30c221b5.jpg", id: 0 },
+            { img: "https://i.pinimg.com/originals/00/82/79/0082797f78fadd08c5bd2926a450667f.png", id: 1 },
+            { img: "https://i.pinimg.com/originals/e9/b6/be/e9b6be54cbf873b14681a21dca530f22.png", id: 2 },
+            { img: "https://c-es.nl/wp-content/uploads/2019/07/2019-07-07-Microsoft-Logo.png", id: 3 },
+            { img: "https://www.aboomi.com/wp-content/uploads/enerjisa.png", id: 4 },
+            { img: "https://www.pendiklitv.com/wp-content/uploads/2018/08/%C4%B0SK%C4%B0-logo.jpg", id: 5 }
+        ]
+}];
 
 
     if ($stateParams.detayId) {
